f8add4b585edb16effc600d8f6bccfb08fbedd59
karen <k.cadenasherrera@gmail.com>
2021-04-03 17:41:54 -0300

    Finalizacion de desafio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListaTodo msg="Crear una nueva tarea"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListaTodo from './components/ListaTodo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListaTodo
   }
 }
 </script>
---
+++ src/components/ListaTodo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <label>Tarea:</label>
+    <input type="text" v-model="inputMensaje" placeholder="Ingresar nueva tarea" />
+    <button @click="agregarTarea">Crear</button>
+    <br>
+    <h1>Lista</h1>
+    <ul>
+      <li v-for="(mensaje, index) in lista" :key="index + 'lista-todo'">{{ mensaje }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListaTodo',
+  data() {
+    return {
+      lista: [],
+      inputMensaje: ''
+    }
+  },
+  methods: {
+    agregarTarea() {
+      if (this.inputMensaje) {
+        this.lista.push(this.inputMensaje);
+        this.inputMensaje = '';
+      }
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 10px;
+}
+a {
+  color: #42b983;
+}
+input {
+  margin: 0 10px;
+}
+</style>
